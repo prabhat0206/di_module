a5a7f75d4fdfb27fd9f714466ac9f7facadaae05
Prabhat Ranjan <Prabhat0206@hotmail.com>
2024-11-06 22:09:05 +0530

    custom module for digicount

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,24 @@ use pyo3::prelude::*;
 use rayon::prelude::*;
 use reqwest;
 
+#[pyclass]
+struct ImageResult {
+    #[pyo3(get)]
+    url: String,
+    #[pyo3(get)]
+    image_binary: Vec<u8>,
+}
+
 #[pyfunction]
-fn load_all_images_from_urls(urls: Vec<String>) -> PyResult<Vec<Vec<u8>>> {
-    let images: Vec<Vec<u8>> = urls.par_iter()
+fn load_all_images_from_urls(urls: Vec<String>) -> PyResult<Vec<ImageResult>> {
+    let images: Vec<ImageResult> = urls.par_iter()
         .filter_map(|url| {
             let response = reqwest::blocking::get(url).ok()?;
-            response.bytes().ok().map(|b| b.to_vec())
+            let bytes = response.bytes().ok()?.to_vec();
+            Some(ImageResult {
+                url: url.clone(),
+                image_binary: bytes,
+            })
         })
         .collect();
 
@@ -17,6 +29,7 @@ fn load_all_images_from_urls(urls: Vec<String>) -> PyResult<Vec<Vec<u8>>> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn digicount_module(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<ImageResult>()?;
     m.add_function(wrap_pyfunction!(load_all_images_from_urls, m)?)?;
     Ok(())
 }
